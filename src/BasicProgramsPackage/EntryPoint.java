7f22972e0249e8dca6400161bf8a086bcadf2f68
Mohammed <mohammednazar@gmail.com>
2023-06-08 23:25:24 +0530

    new changes

--- src/BasicProgramsPackage/EntryPoint.java
+++ src/BasicProgramsPackage/EntryPoint.java
@@ -2,6 +2,11 @@ package BasicProgramsPackage;
 
 public class EntryPoint {
 	public static void main(String args[]) {
-		
+		String str ="Hello world";
+		String rev = " ";
+		for(int i=str.length()-1;i>=0;i--) {
+			rev = rev +str.charAt(i);
+		}
+		System.out.println(rev);
 	}
 }
