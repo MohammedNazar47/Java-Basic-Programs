2f04cba3b53d9ad8dd190ab2ca97c55f879c4133
Mohammed <mohammednazar@gmail.com>
2023-06-08 23:40:08 +0530

    new changes

---
+++ src/BasicProgramsPackage/SampleProgram1.java
@@ -0,0 +1,14 @@
+package BasicProgramsPackage;
+
+public class SampleProgram1 {
+
+	public static void main(String[] args) {
+		int fact = 1;
+		int n =6;
+		for(int i=1;i<=n;i++) {
+			fact = fact*i;
+		}
+			System.out.println(fact);
+	}
+
+}
